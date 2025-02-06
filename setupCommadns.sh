6fb102e0bbce678598fefac44ac487e0678a1f80
EXA-Hub <exa4ads@gmail.com>
2025-02-06 03:18:53 +0200

    remove node_modules

--- setupCommadns.sh
+++ setupCommadns.sh
@@ -1,7 +1,7 @@
 curl -X POST \
 -H "Authorization: Bot {{{{{{{{{{{{{{YOUR_BOT_TOKEN}}}}}}}}}}}}}}" \
 -H "Content-Type: application/json" \
-https://discord.com/api/v10/applications/YOUR_APP_ID/commands \
+https://discord.com/api/v10/applications/{{{{{{{{{{{{{{{{{YOUR_APP_ID}}}}}}}}}}}}}}}}}/commands \
 -d '{
   "name": "ping",
   "description": "Replies with Pong!",
